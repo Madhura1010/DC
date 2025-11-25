79b079dd1dbc8d8b4e725e1906cb46c57d49b82a
Badgujar Madhura Kailas <122299984+Madhura1010@users.noreply.github.com>
2025-11-25 07:33:35 +0530

    Group communication

---
+++ 10.py
@@ -0,0 +1,28 @@
+import socket, threading
+
+clients = []
+
+def handle(client):
+    while True:
+        try:
+            msg = client.recv(1024)
+            if not msg:
+                break
+            for c in clients:
+                if c != client:
+                    c.send(msg)
+        except:
+            break
+    clients.remove(client)
+    client.close()
+
+server = socket.socket()
+server.bind(("0.0.0.0", 12345))
+server.listen(5)
+print("Server started...")
+
+while True:
+    client, addr = server.accept()
+    print("Client connected:", addr)
+    clients.append(client)
+    threading.Thread(target=handle, args=(client,)).start()
